e7737fb40204f3b2addeb8020851f3da1097844a
meenigenikhitha <168175568+meenigenikhitha@users.noreply.github.com>
2024-09-12 19:56:29 +0530

    Update variables.tf

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  default     = "ami-0182f373e66f89c85" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
@@ -56,7 +56,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "gitopskey"
+  default     = "nikki"
 }
 
 variable "instance_name" {
